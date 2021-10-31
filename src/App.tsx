2712bc70aa0470ee402aa0d720c1a75586793e0c
Nahuel Carreiro <ncarreiro@inno-it.es>
2021-10-31 23:42:10 +0100

    Added basic axios test with API /posts full list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useAxios from "axios-hooks";
+import noImage from "./images/no-image.jpg";
+
+interface IPost {
+  id: number;
+  title: string;
+  content: string;
+  lat: string;
+  long: string;
+  image_url: string;
+  created_at: string;
+  updated_at: string;
+}
 
 function App() {
+  const [{ data: posts, loading, error }, refetch] = useAxios(
+    "http://localhost:3000/api/v1/posts"
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <div>Loading Posts...</div>
+      ) : error ? (
+        <div>{error.message}</div>
+      ) : (
+        posts.map((post: IPost) => (
+          <div style={{ margin: 20 }}>
+            <div>{post.title}</div>
+            <img
+              src={post.image_url}
+              style={{ maxWidth: 400 }}
+              onError={(event: any) => (event.target.src = noImage)}
+            />
+            <div>{post.content}</div>
+            <div>{post.lat}</div>
+            <div>{post.long}</div>
+          </div>
+        ))
+      )}
     </div>
   );
 }
