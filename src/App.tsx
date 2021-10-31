f362f25f8b56f04ba533ef4ff5ad984e3b5f8e61
Nahuel Carreiro <ncarreiro@inno-it.es>
2021-11-01 00:02:23 +0100

    Added Axios API config + cleaned Loading and Error code from request.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import "./App.css";
-import useAxios from "axios-hooks";
+import Axios from "axios";
+import useAxios, { configure } from "axios-hooks";
 import noImage from "./images/no-image.jpg";
+
+const axios = Axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+});
+
+configure({ axios });
 
 interface IPost {
   id: number;
@@ -14,48 +21,43 @@ interface IPost {
 }
 
 function App() {
-  const [{ data: posts, loading, error }] = useAxios(
-    "http://localhost:3000/api/v1/posts"
-  );
+  const [{ data: posts, loading, error }] = useAxios("/posts");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <div className="App">
-      {loading ? (
-        <div>Loading Posts...</div>
-      ) : error ? (
-        <div>{error.message}</div>
-      ) : (
-        <table>
-          <thead>
-            <td>Title</td>
-            <td>Image</td>
-            <td>Content</td>
-            <td>Latitude</td>
-            <td>Longitude</td>
-            <td>Created At</td>
-            <td>Updated At</td>
-          </thead>
-          <tbody>
-            {posts.map((post: IPost) => (
-              <tr>
-                <td>{post.title}</td>
-                <td>
-                  <img
-                    src={post.image_url}
-                    style={{ maxWidth: 400 }}
-                    onError={(event: any) => (event.target.src = noImage)}
-                  />
-                </td>
-                <td>{post.content}</td>
-                <td>{post.lat}</td>
-                <td>{post.long}</td>
-                <td>{post.created_at}</td>
-                <td>{post.updated_at}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+      <table>
+        <thead>
+          <td>Title</td>
+          <td>Image</td>
+          <td>Content</td>
+          <td>Latitude</td>
+          <td>Longitude</td>
+          <td>Created At</td>
+          <td>Updated At</td>
+        </thead>
+        <tbody>
+          {posts.map((post: IPost) => (
+            <tr>
+              <td>{post.title}</td>
+              <td>
+                <img
+                  src={post.image_url}
+                  style={{ maxWidth: 400 }}
+                  onError={(event: any) => (event.target.src = noImage)}
+                />
+              </td>
+              <td>{post.content}</td>
+              <td>{post.lat}</td>
+              <td>{post.long}</td>
+              <td>{post.created_at}</td>
+              <td>{post.updated_at}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
