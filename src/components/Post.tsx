854ce8bf15a58db1dd1d22292102df4db3c51a20
Nahuel Carreiro <ncarreiro@inno-it.es>
2021-11-01 02:53:58 +0100

    Added Material Icons + new initial EditPost Panel + open / close panel configuration.

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,6 +4,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import CardActionArea from "@mui/material/CardActionArea";
+import Drawer from "@mui/material/Drawer";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 
@@ -12,6 +13,8 @@ import IPost from "../types/Post";
 
 // NO IMAGE FALLBACK
 import noImage from "../images/no-image.jpg";
+import { useState } from "react";
+import EditPost from "./EditPost";
 
 const Post = ({
   id,
@@ -22,81 +25,104 @@ const Post = ({
   long,
   created_at,
   updated_at,
-}: IPost) => (
-  <Grid item>
-    <Card sx={{ maxWidth: 345 }} data-testid={`post-${id}`}>
-      <CardActionArea onClick={() => console.log("Card onClick action")}>
-        <CardMedia
-          data-testid={`post-${id}-image`}
-          component="img"
-          height="140"
-          alt={title}
-          image={image_url}
-          onError={(event: any) => (event.target.src = noImage)}
+}: IPost) => {
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  return (
+    <Grid item>
+      <Card sx={{ maxWidth: 345 }} data-testid={`post-${id}`}>
+        <CardActionArea onClick={() => setShowDrawer(true)}>
+          <CardMedia
+            data-testid={`post-${id}-image`}
+            component="img"
+            height="140"
+            alt={title}
+            image={image_url}
+            onError={(event: any) => (event.target.src = noImage)}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {content}
+            </Typography>
+            <Divider sx={{ my: 2 }} />
+            <Grid container>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  Latitude:
+                </Typography>
+              </Grid>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  {lat}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Divider sx={{ my: 2 }} />
+            <Grid container>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  Longitude:
+                </Typography>
+              </Grid>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  {long}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Divider sx={{ my: 2 }} />
+            <Grid container>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  Created at:
+                </Typography>
+              </Grid>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  {created_at}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Divider sx={{ my: 2 }} />
+            <Grid container sx={{ mb: 1 }}>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  Last updated:
+                </Typography>
+              </Grid>
+              <Grid item xs>
+                <Typography variant="body2" color="text.secondary">
+                  {updated_at}
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <Drawer
+        anchor="right"
+        open={showDrawer}
+        onClose={() => setShowDrawer(false)}
+      >
+        <EditPost
+          {...{
+            id,
+            title,
+            image_url,
+            content,
+            lat,
+            long,
+            created_at,
+            updated_at,
+          }}
+          onCancel={() => setShowDrawer(false)}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {content}
-          </Typography>
-          <Divider sx={{ my: 2 }} />
-          <Grid container>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                Latitude:
-              </Typography>
-            </Grid>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                {lat}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider sx={{ my: 2 }} />
-          <Grid container>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                Longitude:
-              </Typography>
-            </Grid>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                {long}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider sx={{ my: 2 }} />
-          <Grid container>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                Created at:
-              </Typography>
-            </Grid>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                {created_at}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider sx={{ my: 2 }} />
-          <Grid container sx={{ mb: 1 }}>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                Last updated:
-              </Typography>
-            </Grid>
-            <Grid item xs>
-              <Typography variant="body2" color="text.secondary">
-                {updated_at}
-              </Typography>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  </Grid>
-);
+      </Drawer>
+    </Grid>
+  );
+};
 
 export default Post;
